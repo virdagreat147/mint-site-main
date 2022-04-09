8ed185655d201453474c34ce849c0992a048f890
coderboi4055 <virarora41@gmail.com>
2022-04-09 19:25:19 -0400

    change

--- spp_mint_site/src/App.js
+++ spp_mint_site/src/App.js
@@ -2,6 +2,7 @@ import Background from './bg.png';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router} from 'react-router-dom';
+import Coin from '.coin.gif'
 
 function App() {
   return (
