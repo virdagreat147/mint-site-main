a482d37eb9459c8933b738c790c90e506c9c8010
coderboi4055 <virarora41@gmail.com>
2022-04-09 19:32:32 -0400

    change

--- spp_mint_site/src/App.js
+++ spp_mint_site/src/App.js
@@ -2,13 +2,15 @@ import Background from './bg.png';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router} from 'react-router-dom';
-import Coin from '.coin.gif'
+import Coin from '.coin.gif';
 
 function App() {
   return (
     <div className="App" style={{backgroundImage: `url(${Background})`,backgroundSize: "cover"}}>
       <header className="App-header">
       <Router><Navbar /></Router>
+      <div><Coin />
+      </div>
       </header>
     </div>
   );
